bfc5bda4f946aecc0bf185a93885df2a74c17e46
TheLastSlime974 <monjeu029@gmail.com>
2026-01-13 11:10:28 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// Sélection des éléments
 const bouton = document.getElementById("btn");
 const message = document.getElementById("message");
 
+// Action au clic
 bouton.addEventListener("click", () => {
     message.textContent = "Bravo 🎉 Tu utilises JavaScript !";
 });
